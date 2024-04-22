a78fe914382ce43dea823190a25f612a64485063
StonesRocks <68532873+StonesRocks@users.noreply.github.com>
2024-04-22 12:59:46 +0200

    test

--- ProjectASParagus/Objects/Booking.cs
+++ ProjectASParagus/Objects/Booking.cs
@@ -2,5 +2,22 @@
 {
     public class Booking
     {
+        public int BookingId { get; set; } = int.MinValue;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.MinValue;
+        public int PartySize { get; set; } = int.MinValue;
+
+        public Booking(string name, string email, string phone, DateTime date, int partySize)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Date = date;
+            PartySize = partySize;
+        }
+
+        public Booking(){}
     }
 }
